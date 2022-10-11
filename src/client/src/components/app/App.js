d7651c313caef8bc0d8fb5f9f01fdc06515913e1
Pouria Mirebrahimi <pouria.mirebrahimi@gmail.com>
2022-10-12 01:57:01 +0330

    Adding Router in client

--- src/client/src/components/app/App.js
+++ src/client/src/components/app/App.js
@@ -1,10 +1,33 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+// import pages
 import SignIn from "../pages/signin"
 
 function App() {
   return (
-    <div className="App">
-      <SignIn />
-    </div>
+    <Fragment>
+      <div className="App">
+
+        <Router>
+          <Switch>
+
+            <Route exact path=
+              {
+                [
+                  "/"
+                ]
+              } render={() => (
+                <Fragment>
+                  <SignIn />
+                </Fragment>
+              )} />
+
+          </Switch>
+        </Router>
+
+      </div>
+    </Fragment>
   )
 }
 
